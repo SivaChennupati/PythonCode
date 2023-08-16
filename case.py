78da99a7371a06029a830f6069b166ea4cb93dcc
SivaChennupati <india119@hotmail.com>
2023-08-16 15:22:18 -0400

    Update case.py
    Added 4th option

--- case.py
+++ case.py
@@ -3,12 +3,12 @@ def choose_case_example(option):
         1: "Option 1 selected",
         2: "Option 2 selected",
         3: "Option 3 selected",
-        4: "Option 3 selected"
+        4: "Option 4 selected"
     }
 
     result = options.get(option, "Invalid option")
     print(result)
 
 
-user_option = int(input("Enter an option (1, 2, or 3): "))
+user_option = int(input("Enter an option (1, 2, 3 or 4): "))
 choose_case_example(user_option)
